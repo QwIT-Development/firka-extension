6a52c51ece74cb541635f59e8ef3704a84ac7ec7
Zan1456 <62830223+Zan1456@users.noreply.github.com>
2025-06-13 13:59:17 +0200

    .

--- tools/createTemplate.js
+++ tools/createTemplate.js
@@ -111,6 +111,7 @@ const createTemplate = {
             { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
             { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: true },
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap' },
+            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap' },
             { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons+Round' }
         ];
         
